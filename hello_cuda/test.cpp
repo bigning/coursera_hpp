987899bb42c0f26e3d7e6d7b5d92d4d6e83ef20c
Ning Wang <bigning1118@gmail.com>
2016-01-15 17:12:54 -0500

    hello_cuda print out more properties

--- hello_cuda/test.cpp
+++ hello_cuda/test.cpp
@@ -6,4 +6,20 @@ int main() {
     int device_count = 0;
     cudaGetDeviceCount(&device_count);
     std::cout << "hello cuda " << device_count << std::endl;
+
+    if (device_count == 0) {
+        return 0;
+    }
+
+    for (int i = 0; i < device_count; i++) {
+        cudaDeviceProp device_prop;
+        cudaGetDeviceProperties(&device_prop, i);
+
+        std::cout << "Device " << i << " name:" << device_prop.name << std::endl;
+        std::cout << "\tcomputtational capablities: " << device_prop.major << "." << device_prop.minor << std::endl;
+        std::cout << "\tglobal memory size:" << device_prop.totalGlobalMem << std::endl;
+        std::cout << "\tblock dimension:" << device_prop.maxThreadsDim[0] << " x " << device_prop.maxThreadsDim[1] << std::endl;
+    }
+
+    return 1;
 }
