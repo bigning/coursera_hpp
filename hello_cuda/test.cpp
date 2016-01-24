8f9485dbcf55bebb5391b5cad6fc28022e919684
Ning Wang <nw932@gpu-24-11.local>
2016-01-24 17:28:29 -0500

    print more device information

--- hello_cuda/test.cpp
+++ hello_cuda/test.cpp
@@ -19,6 +19,10 @@ int main() {
         std::cout << "\tcomputtational capablities: " << device_prop.major << "." << device_prop.minor << std::endl;
         std::cout << "\tglobal memory size:" << device_prop.totalGlobalMem << std::endl;
         std::cout << "\tblock dimension:" << device_prop.maxThreadsDim[0] << " x " << device_prop.maxThreadsDim[1] << std::endl;
+	std::cout << "\tmax grid size:" << device_prop.maxGridSize[0] << ", " << device_prop.maxGridSize[1] << ", " << device_prop.maxGridSize[2] << std::endl; 
+	std::cout << "\tmax threads per block:" << device_prop.maxThreadsPerBlock << std::endl;
+	std::cout << "\tmulti processor count:"  << device_prop.multiProcessorCount << std::endl;
+	std::cout << "\tshare memory per block: " << device_prop.sharedMemPerBlock << std::endl;
     }
 
     return 1;
